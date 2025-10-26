d572e520d121667b22827a25270e80cac90bbfcf
Haseeb0128 <haseebn2868@gmail.com>
2025-10-26 23:02:23 +0500

    product details

---
+++ src/components/product-details/ProductDetailsClient.tsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const ProductDetailsClient = () => {
+  return <div className="min-h-screen bg-white">ProductDetailsClient</div>;
+};
+
+export default ProductDetailsClient;
